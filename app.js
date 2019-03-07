5c2cb28b20a77ae691e93bb8e464fb21940903bc
CristinaG <cristina.guerrero@outlook.com>
2019-03-07 21:31:15 +0000

    add initial routes layout and create new campground

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let campgrounds = [
+        {name: "Black Mountain", image: "https://images.unsplash.com/photo-1479741044197-d28c298f8c77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+        {name: "Sierra Cazorla", image: "https://images.unsplash.com/photo-1504591504549-8ce1589ea6f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"},
+        {name: "Sierra Morena", image: "https://images.unsplash.com/photo-1522031153701-b3eba74004e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"}
+];
 
 app.get("/", (req, res) => {
-    res.send("This will be the landing page");
-})
+    res.render("landing");
+});
 
+app.get("/campgrounds", (req, res) => {
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+    // get data from a form and add it to campgrounds array
+    const newName = req.body.name;
+    const newImage = req.body.image;
+    const newCamp = {name: newName, image: newImage};
+    campgrounds.push(newCamp);
+    // redirect to campgrounds page
+    res.redirect("/campgrounds");
+    
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new");
+});
 
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log("The YelpCamp Server Has Started");
